2aab035fda09a73923f6af7ef5e94032359c1209
Pooya Hatami <pooya@node-1w7jr9qro06swsy8h29me26e8.ipv6.telus.net>
2018-07-31 15:39:24 -0700

    done

--- IOS-Swift-TouchID/ViewController.swift
+++ IOS-Swift-TouchID/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func authWithTouchID(_ sender: Any) {
+        // 1
+        let context = LAContext()
+        var error: NSError?
+        
+        // 2
+        // check if Touch ID is available
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // 3
+            let reason = "Authenticate with Touch ID"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason, reply:
+                {(succes, error) in
+                    // 4
+                    if succes {
+                        self.showAlertController("Touch ID Authentication Succeeded")
+                    }
+                    else {
+                        self.showAlertController("Touch ID Authentication Failed")
+                    }
+                    })
+        }
+            // 5
+        else {
+            showAlertController("Touch ID not available")
+        }
+    }
+    
+    func showAlertController(_ message: String) {
+        let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
